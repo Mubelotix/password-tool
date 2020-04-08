d869b4973609bb35fe17669f48971ae33074bd0d
Mubelotix <mubelotix@gmail.com>
2020-04-08 15:13:01 +0200

    added replacement passwords

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,18 @@ mod test {
         assert_eq!(&extract_domain_name("http://mubelotix.dev/passwords").unwrap(), "mubelotix.dev");
         assert_eq!(&extract_domain_name("https://test.mubelotix.dev/index.html").unwrap(), "mubelotix.dev");
     }
+
+    #[test]
+    fn backward_compatibility_test() {
+        assert_eq!(&generate_password("test", "unknown.unknown", false, false, false), "00c7dfebc7943SOD");
+        assert_eq!(&generate_password("test", "unknown.unknown", false, false, true), "00c7dfebc769*_BQ");
+        assert_eq!(&generate_password("test", "unknown.unknown", false, true, false), "0199223235199105");
+        assert_eq!(&generate_password("test", "unknown.unknown", false, true, true), "0199223235199105");
+        assert_eq!(&generate_password("test", "unknown.unknown", true, false, false), "00c7dfebc769bc042ad64ed60d9447dcaeaf7e9cabef943SOD");
+        assert_eq!(&generate_password("test", "unknown.unknown", true, false, true), "00c7dfebc769bc042ad64ed60d9447dcaeaf7e9cabef@*_BQF");
+        assert_eq!(&generate_password("test", "unknown.unknown", true, true, false), "01992232351991051884422147821413148712201741751261");
+        assert_eq!(&generate_password("test", "unknown.unknown", true, true, true), "01992232351991051884422147821413148712201741751261");
+    }
 }
 
 fn extract_domain_name(mut url: &str) -> Option<String> {
@@ -60,7 +72,7 @@ fn generate_password(master_password: &str, domain: &str, big: bool, only_number
     let password_size = if big {
         50
     } else {
-        15
+        16
     };
     
     let mut hasher = Sha3_512::new();
@@ -72,7 +84,11 @@ fn generate_password(master_password: &str, domain: &str, big: bool, only_number
     let generated_password = hasher.result();
 
     let mut generated_password: String = if !only_numbers {
-        hex::encode(generated_password[..password_size/2-3].to_vec())
+        if special_chars && !big {
+            hex::encode(generated_password[..password_size/2-2].to_vec())
+        } else {
+            hex::encode(generated_password[..password_size/2-3].to_vec())
+        }
     } else {
         let mut generated_password2 = String::new();
         for n in generated_password {
@@ -83,7 +99,11 @@ fn generate_password(master_password: &str, domain: &str, big: bool, only_number
     };
 
     if special_chars {
-        generated_password.push_str("@*_BQF");
+        if big {
+            generated_password.push_str("@*_BQF");
+        } else {
+            generated_password.push_str("*_BQ");
+        }
     } else {
         generated_password.push_str("943SOD");
     }
@@ -95,6 +115,7 @@ enum Page {
     EnterMainPassword,
     EnterUrl,
     DisplayGeneratedPassword,
+    MorePasswords,
     Sorry(String)
 }
 
@@ -103,14 +124,15 @@ struct Model {
     main_password: String,
     url: String,
     domain: String,
-    generated_password: String,
+    generated_passwords: [String; 6],
+    accessible_password: usize,
     page: Page,
 }
 
 enum Msg {
     Next,
     SecondaryButton,
-    CopyPassword,
+    CopyPassword(usize),
 }
 
 impl Component for Model {
@@ -122,7 +144,8 @@ impl Component for Model {
             page: Page::EnterMainPassword,
             main_password: String::new(),
             url: String::new(),
-            generated_password: String::new(),
+            generated_passwords: [String::new(),String::new(),String::new(),String::new(),String::new(),String::new()],
+            accessible_password: 0,
             domain: String::new(),
         }
     }
@@ -153,32 +176,51 @@ impl Component for Model {
                     // Provisory: because of bad wasm support on mobile
                     self.url = InputElement::try_from(document().get_element_by_id("url_input").unwrap()).unwrap().raw_value();
 
-                    self.domain = extract_domain_name(&self.url).unwrap_or(String::from("unknow.unknow"));
+                    self.domain = extract_domain_name(&self.url).unwrap_or(String::from("unknown.unknown"));
 
-                    self.generated_password = generate_password(&self.main_password, &self.domain, true, false, true);
+                    self.generated_passwords[0] = generate_password(&self.main_password, &self.domain, true, false, true);
+                    self.generated_passwords[1] = generate_password(&self.main_password, &self.domain, true, false, false);
+                    self.generated_passwords[2] = generate_password(&self.main_password, &self.domain, true, true, true);
+                    self.generated_passwords[3] = generate_password(&self.main_password, &self.domain, false, false, true);
+                    self.generated_passwords[4] = generate_password(&self.main_password, &self.domain, false, false, false);
+                    self.generated_passwords[5] = generate_password(&self.main_password, &self.domain, false, true, true);
                     
                     self.page = Page::DisplayGeneratedPassword;
+                    self.accessible_password = 0;
                     true },
-                Page::DisplayGeneratedPassword => {self.page = Page::Sorry("I didn't implemented this for now".to_string()); true},
-                _ => false,
-            },
-            Msg::CopyPassword => {
-                js! { @(no_return)
-                    var el = document.createElement("textarea");
-                    el.value = @{&self.generated_password};
-                    el.setAttribute("readonly", "");
-                    el.style = {position: "absolute", left: "-9999px"};
-                    document.body.appendChild(el);
-                    el.select();
-                    document.execCommand("copy");
-                    document.body.removeChild(el);
+                Page::DisplayGeneratedPassword => {self.page = Page::MorePasswords; true},
+                Page::Sorry(_) => false,
+                Page::MorePasswords => {
+                    {self.page = Page::Sorry("There is no page here!".to_string()); true}
                 }
-                false
+            },
+            Msg::CopyPassword(idx) => {
+                if idx <= self.accessible_password {
+                    js! { @(no_return)
+                        var el = document.createElement("textarea");
+                        el.value = @{&self.generated_passwords[idx]};
+                        el.setAttribute("readonly", "");
+                        el.style = {position: "absolute", left: "-9999px"};
+                        document.body.appendChild(el);
+                        el.select();
+                        document.execCommand("copy");
+                        document.body.removeChild(el);
+                    }
+                    if self.accessible_password == idx {
+                        self.accessible_password += 1;
+                    }
+                } else {
+                    js! { @(no_return)
+                        alert("Error: this password can't be copied now");
+                    }
+                }
+                true
             },
             Msg::SecondaryButton => match self.page {
                 Page::EnterMainPassword => {self.page = Page::Sorry(String::from("Unimplemented password generation")); true},
                 Page::EnterUrl => {self.page = Page::EnterMainPassword; true},
                 Page::DisplayGeneratedPassword => {self.page = Page::EnterUrl; true},
+                Page::MorePasswords => {self.page = Page::DisplayGeneratedPassword; true},
                 Page::Sorry(_) => {self.page = Page::EnterMainPassword; true},
             }
         }
@@ -223,13 +265,53 @@ impl Component for Model {
             Page::DisplayGeneratedPassword => {
                 html! {
                     <main>
-                        {format!("Your password for the website {} has been generated successfully!", self.domain)}<br/>
+                        {
+                            if self.domain == "unknown.unknown" {
+                                String::from("WARNING: Failed to parse the URL. The domain has been replaced by the wildcard domain: \"unknown.unknown\". You should avoid using this password, go back and enter the domain name again.")
+                            } else {
+                                format!("Your password for the website {} has been generated successfully!", self.domain)
+                            }
+                            
+                        }<br/>
                         <br/>
-                        <button onclick=self.link.callback(|_| Msg::CopyPassword)>{"Copy password"}</button><br/>
+                        <button onclick=self.link.callback(|_| Msg::CopyPassword(0))>{"Copy password"}</button><br/>
                         <br/>
-                        {"In case where your password is rejected because of requirements, please try the button below."}<br/>
+                        {"If your password is rejected, please try the \"Help\" button below."}<br/>
                         <br/>
                         <button onclick=self.link.callback(|_| Msg::Next)>{ "Help" }</button><br />
+                        <br />
+                        <button onclick=self.link.callback(|_| Msg::SecondaryButton)>{ "Back" }</button>
+                    </main>
+                }
+            },
+            Page::MorePasswords => {
+                let mut buttons = Vec::new();
+                for idx in 0..6 {
+                    buttons.push(if self.accessible_password >= idx {
+                        html! {
+                            <div><button onclick=self.link.callback(move |_| Msg::CopyPassword(idx))>{format!("Password {}", idx)}</button><br/><br/></div>
+                        }
+                    } else {
+                        html! {
+                            <div><button class="disabled_button">{"Try the password above"}</button><br/><br/></div>
+                        }
+                    });
+                };
+                html! {
+                    <main>
+                        {"Try every password of this list in the correct order."}<br/>
+                        <br/>
+                        {
+                            for buttons
+                        }
+                        {
+                            if self.accessible_password > 5 {
+                                "If none of these passwords above is working and you are trying to sign up, please find a password by yourself. You can send me an email (at mubelotix@gmail.com) to report the website, and I will see if I can do something. This website is weakly configured, and I can't do anything for that. If you are trying to sign in, either you didn't enter the correct password or the correct URL, or you created a password by yourself on this website."
+                            } else {
+                                ""
+                            }
+                        }
+                        <br />
                         <br />
                         <button onclick=self.link.callback(|_| Msg::SecondaryButton)>{ "Back" }</button>
                     </main>
